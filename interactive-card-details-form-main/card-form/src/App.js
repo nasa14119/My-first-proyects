7f27031e5c2432539c9377337d796ae0818dba44
nasa14119 <nico.amaya0205@gmail.com>
2022-11-06 16:52:28 -0600

    finish work

--- interactive-card-details-form-main/card-form/src/App.js
+++ interactive-card-details-form-main/card-form/src/App.js
@@ -11,7 +11,6 @@ function App() {
     name:false, 
   });
   const [haveAcces, LevelAcces] = useState(false)
-  const [test, settest] = useState(false); 
   const [error, setError] = useState({
     number:"",
     month: "", 
@@ -42,19 +41,20 @@ function App() {
         setError(prev => ({prev,[key] :""}))
       }
     }
-    if(Object.values(acces.current).every(value=> value === true)){
+    if(!acces.current.month){
+      setError(prev => ({...prev, month:"error digits must be ej. 01"}))
+    }
+    if(Object.values(acces.current).every(value => value === true)){
       LevelAcces(true)
     }
   }
   const HaveAccesAnimation = () => {
     return (
       <div className="haveAcces">
-        <span className="content">
-          <svg width="80" height="80" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="40" cy="40" r="40" fill="url(#a)"/><path d="M28 39.92 36.08 48l16-16" stroke="#fff" stroke-width="3"/><defs><linearGradient id="a" x1="-23.014" y1="11.507" x2="0" y2="91.507" gradientUnits="userSpaceOnUse"><stop stop-color="#6348FE"/><stop offset="1" stop-color="#610595"/></linearGradient></defs></svg>
+          <svg width="80" height="80" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="40" cy="40" r="40" fill="url(#a)"/><path d="M28 39.92 36.08 48l16-16" stroke="#fff" strokeWidth="3"/><defs><linearGradient id="a" x1="-23.014" y1="11.507" x2="0" y2="91.507" gradientUnits="userSpaceOnUse"><stop stopColor="#6348FE"/><stop offset="1" stopColor="#610595"/></linearGradient></defs></svg>
           <h2>Thank You!</h2>
           <p>We've added your card</p>
-          <button>Continue</button>
-        </span> 
+          <button onClick={()=> LevelAcces(false)}>Continue</button>
       </div>
     )
   }
@@ -216,14 +216,6 @@ function App() {
           <HaveAccesAnimation />
         )}
       </div>
-      <button
-        style={{ position: "fixed", bottom: "0", left: "0" }}
-        onClick={() => {
-          settest((current) => !current);
-        }}
-      >
-        Debugh
-      </button>
     </div>
   );
 }
